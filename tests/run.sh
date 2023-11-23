ab6d7250e965a9856bb351d830b6de4901be8c04
Prem chavhan <pchavhan@logiquad.com>
2023-11-23 12:45:47 +0530

    fix

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker-compose up -d;
-sleep 100s;
+sleep 200s;
 
 docker-compose exec -T worker bash -c "export DJANGO_SUPERUSER_EMAIL=${ADMIN_EMAIL} && export DJANGO_SUPERUSER_PASSWORD=${ADMIN_PASSWORD} && ./manage.py createsuperuser --noinput"
