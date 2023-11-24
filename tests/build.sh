4668e982131b36d878e7bb0840ad6e0ecc9ddf7e
Prem Chavhan <pchavhan@logiquad.com>
2023-11-24 13:14:32 +0530

    Updated fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
+mkdir glitchtip-frontend
+git clone https://gitlab.com/glitchtip/glitchtip-frontend.git litchtip-frontend/
+cd glitchtip-frontend/
+npm ci --force
+npm run build-prod
+cd ..
+mkdir dist
+cp -rf glitchtip-frontend/dist/glitchtip-frontend/* ./dist/
+rm -rf glitchtip-frontend/
 
-
-# git clone https://github.com/rh-cssre/glitchtip-frontend.git
-# cd glitchtip-frontend/
-# npm ci install --force
-# npm run build-prod
-# cd ..
-# mkdir dist
-# cp -rf glitchtip-frontend/dist/glitchtip-frontend/* ./dist/
-# rm -rf glitchtip-frontend/
-
-# sed -i 's/ARG IS_CI/ARG IS_CI="True"/g' Dockerfile
-# sed -i 's/ARG COLLECT_STATIC/ARG COLLECT_STATIC="True"/g' Dockerfile
+sed -i 's/ARG IS_CI/ARG IS_CI="True"/g' Dockerfile
+sed -i 's/ARG COLLECT_STATIC/ARG COLLECT_STATIC="True"/g' Dockerfile
 
 docker buildx build . --output type=docker,name=elestio4test/glitchtip:latest | docker load
